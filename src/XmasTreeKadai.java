0f96aa6344bc0ce03f1010262fbac466cbfd14f5
22FI024HaruyaOno <22fi024@ms.dendai.ac.jp>
2024-04-26 12:51:13 +0900

    finish

--- src/XmasTreeKadai.java
+++ src/XmasTreeKadai.java
@@ -2,31 +2,51 @@ import java.util.Scanner;
 
 public class XmasTreeKadai {
 
+    static int N;
+    static int W;
+    static int L;
 	public static void main(String[] args) {
-		int i=0;
-        int first,second;
-		while(i<10) {
-			Scanner scan = new Scanner(System.in);
-            
-			String str = scan.next();
-			System.out.println("最初のトークンは: " + str);
-            System.out.println("文字を分割すると・・・");
-            String[] word = str.split(",");//splitメソッドの引数に、どこの文字で分割するか指定します。この場合は空白文字。
-            for(String w:word) {
-                System.out.println(w);
+        System.out.println("最大の葉の横幅,幹の横幅,幹の長さの順で記入してください");
+		Scanner scan = new Scanner(System.in);
+		String str = scan.next();
+		System.out.println("クリスマスツリーのトークンは: " + str);
+        System.out.println("文字を分割すると・・・");
+        String[] word = str.split(",");//splitメソッドの引数に、どこの文字で分割するか指定します。この場合は空白文字。
+        for(String w:word) {
+            System.out.println(w);
+        }
+        N = Integer.parseInt(word[0]);
+        W = Integer.parseInt(word[1]);
+        L = Integer.parseInt(word[2]);
+        System.out.println("最大の葉の横幅"+N);
+        System.out.println("幹の横幅"+W);
+        System.out.println("幹の長さ"+L);
+        System.out.println("クリスマスツリーを描画します");
+        for (int j = 0; j < N; j++) {
+            for (int i = 0; i <= N-j; i++) {
+                System.out.print("+");
             }
-            System.out.println("word[0]"+word[0]);
-            System.out.println("word[1]"+word[1]);
-            System.out.println("word[2]"+word[2]);
-            System.out.println("word[3]"+word[3]);
-            //19,3,7,c
-            //first = Integer.parseInt(str);
-            System.out.println("----次の数字の入力をお願いします ");
-		}
 
+            for (int i = 0; i <= j*2; i++) {
+                System.out.print("*");
+            }
 
+            for (int i = 0; i <= N-j; i++) {
+                System.out.print("+");
+            }
+            System.out.print("\n");
+        }
+
+        for (int j = 0; j < L; j++) {
+
+            for (int i = 0; i < N; i++) {
+                System.out.print(" ");
+            }
+            for (int i = 0; i < 3; i ++) {
+                System.out.print("*");
+            }
+
+            System.out.print("\n");
+        }
 	}
 }
-
-//  課題    キーボードから2つの数字を打ち込む
-//     その足し算結果を、出力する。
